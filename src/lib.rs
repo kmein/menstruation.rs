f5fa70bc2b86892ea0a42b16d1265049a142d5f2
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2018-12-19 12:07:14 +0100

    love rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,60 @@
 extern crate scraper;
+extern crate regex;
+extern crate ansi_term;
 
-use scraper::{Html, Selector};
-use std::collections::HashSet;
+use std::fmt;
+use std::collections;
+
+
+mod utility {
+    pub fn partition<A, P, I>(predicate: P, xs: I) -> (Vec<A>, Vec<A>)
+    where
+        P: Fn(&A) -> bool,
+        I: Iterator<Item = A>,
+    {
+        let mut toepfchen = Vec::new();
+        let mut kroepfchen = Vec::new();
+        for x in xs {
+            if predicate(&x) {
+                toepfchen.push(x);
+            } else {
+                kroepfchen.push(x);
+            }
+        }
+        (toepfchen, kroepfchen)
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Cents(u64);
 
+impl Cents {
+    fn from_euro(euro: f32) -> Self {
+        Cents((euro * 100f32) as u64)
+    }
+}
+
+impl fmt::Display for Cents {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let Cents(total_cents) = self;
+        let euros = total_cents / 100;
+        let cents = total_cents % 100;
+        write!(f, "{},{:02} €", euros, cents)
+    }
+}
+
 #[derive(Debug)]
 pub struct MenuResponse {
     groups: Vec<MealGroup>,
+}
+
+impl fmt::Display for MenuResponse {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for group in &self.groups {
+            write!(f, "{}", group);
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
@@ -17,13 +63,57 @@ struct MealGroup {
     meals: Vec<Meal>,
 }
 
+impl fmt::Display for MealGroup {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(
+            f,
+            "{}",
+            ansi_term::Style::new().bold().paint(
+                &self.name.to_uppercase(),
+            )
+        );
+        for meal in &self.meals {
+            write!(f, "{}", meal);
+        }
+        writeln!(f, "");
+        Ok(())
+    }
+}
+
 #[derive(Debug)]
 struct Meal {
     name: String,
     color: MealColor,
-    tags: HashSet<MealTag>,
+    tags: collections::HashSet<MealTag>,
     price: Option<MealPrice>,
-    allergens: HashSet<String>,
+    allergens: collections::HashSet<String>,
+}
+
+impl fmt::Display for Meal {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fn to_ansi(color: &MealColor) -> ansi_term::Colour {
+            match color {
+                MealColor::Green => ansi_term::Colour::Green,
+                MealColor::Red => ansi_term::Colour::Red,
+                MealColor::Yellow => ansi_term::Colour::Yellow,
+            }
+        }
+        writeln!(
+            f,
+            "[{}] {} {}",
+            if let Some(price) = &self.price {
+                format!("{}", price.student)
+            } else {
+                "      ".to_string()
+            },
+            to_ansi(&self.color).paint(&self.name),
+            self.tags
+                .iter()
+                .map(|tag| format!("{}", tag))
+                .collect::<Vec<_>>()
+                .join(" ")
+        )
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash)]
@@ -53,6 +143,22 @@ pub enum MealTag {
     ClimateFriendly,
 }
 
+impl fmt::Display for MealTag {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            ansi_term::Style::new().italic().paint(match self {
+                MealTag::Vegetarian => "vegetarisch",
+                MealTag::Vegan => "vegan",
+                MealTag::Organic => "bio",
+                MealTag::SustainableFishing => "nachhaltig",
+                MealTag::ClimateFriendly => "öko",
+            })
+        )
+    }
+}
+
 impl From<&str> for MealTag {
     fn from(uri: &str) -> Self {
         match uri {
@@ -61,7 +167,7 @@ impl From<&str> for MealTag {
             "/vendor/infomax/mensen/icons/18.png" => MealTag::Organic,
             "/vendor/infomax/mensen/icons/38.png" => MealTag::SustainableFishing,
             "/vendor/infomax/mensen/icons/43.png" => MealTag::ClimateFriendly,
-            _ => panic!("Canno parse URI: {}", uri),
+            _ => panic!("Cannot parse URI: {}", uri),
         }
     }
 }
@@ -73,62 +179,50 @@ struct MealPrice {
     guest: Cents,
 }
 
-fn partition<A, P, I>(predicate: P, xs: I) -> (Vec<A>, Vec<A>)
-where
-    P: Fn(&A) -> bool,
-    I: Iterator<Item = A>,
-{
-    let mut toepfchen = Vec::new();
-    let mut kroepfchen = Vec::new();
-    for x in xs {
-        if predicate(&x) {
-            toepfchen.push(x);
-        } else {
-            kroepfchen.push(x);
-        }
-    }
-    (toepfchen, kroepfchen)
-}
+pub fn extract_response(html: &str) -> Result<MenuResponse, &str> {
+    let groups_selector = scraper::Selector::parse(".splGroupWrapper").unwrap();
+    let group_name_selector = scraper::Selector::parse(".splGroup").unwrap();
+    let meal_selector = scraper::Selector::parse(".splMeal").unwrap();
+    let icon_selector = scraper::Selector::parse("img.splIcon").unwrap();
+    let meal_name_selector = scraper::Selector::parse("span.bold").unwrap();
+    let price_selector = scraper::Selector::parse("div.text-right").unwrap();
+    let allergen_selector = scraper::Selector::parse(".toolt").unwrap();
 
-pub fn extract_response(html: &str) -> MenuResponse {
-    let groups_selector = Selector::parse(".splGroupWrapper").unwrap();
-    let group_name_selector = Selector::parse(".splGroup").unwrap();
-    let meal_selector = Selector::parse(".splMeal").unwrap();
-    let icon_selector = Selector::parse("img.splIcon").unwrap();
-    let meal_name_selector = Selector::parse("span.bold").unwrap();
-    let price_selector = Selector::parse("div.text-right").unwrap();
-
-    let dom = Html::parse_fragment(html);
-
-    println!("{:?}", dom);
+    let dom = scraper::Html::parse_fragment(html);
 
     let mut groups = Vec::new();
     for group_html in dom.select(&groups_selector) {
         let group_name = group_html
             .select(&group_name_selector)
             .next()
-            .unwrap()
+            .expect("No group name found")
             .inner_html();
         let mut meals = Vec::new();
         for meal_html in group_html.select(&meal_selector) {
             let icons_html = meal_html.select(&icon_selector).map(|img| {
-                img.value().attr("src").unwrap()
+                img.value().attr("src").expect("Icon has no src")
             });
-            let (color_htmls, tag_htmls) = partition(|&src| src.contains("ampel"), icons_html);
+            let (color_htmls, tag_htmls) =
+                utility::partition(|&src| src.contains("ampel"), icons_html);
             let color = MealColor::from(color_htmls[0]);
             let tags = tag_htmls.iter().map(|&src| MealTag::from(src)).collect();
             let meal_name = meal_html
                 .select(&meal_name_selector)
                 .next()
-                .unwrap()
-                .inner_html();
+                .expect("No meal name found")
+                .inner_html()
+                .trim()
+                .to_string();
             let price = if let Some(price_raw) = meal_html.select(&price_selector).next() {
                 let prices: Vec<_> = price_raw
                     .inner_html()
-                    .replace("€ ", "")
+                    .replace("€", "")
+                    .trim()
                     .replace(",", ".")
                     .split("/")
-                    .map(|p| Cents((p.parse::<f64>().unwrap() * 100f64) as u64))
+                    .map(|p| {
+                        Cents::from_euro(p.parse::<f32>().expect("Could not parse price"))
+                    })
                     .collect();
                 Some(MealPrice {
                     student: prices[0].clone(),
@@ -138,13 +232,28 @@ pub fn extract_response(html: &str) -> MenuResponse {
             } else {
                 None
             };
-            // allergens here
+            let allergens = {
+                let parenthesized = regex::Regex::new(r"\((.*)\)").unwrap();
+                let allergens_html = meal_html
+                    .select(&allergen_selector)
+                    .next()
+                    .expect("No allergens found")
+                    .inner_html();
+                if let Some(captures) = parenthesized.captures(&allergens_html) {
+                    String::from(&captures[1])
+                        .split(", ")
+                        .map(String::from)
+                        .collect()
+                } else {
+                    collections::HashSet::new()
+                }
+            };
             meals.push(Meal {
                 name: meal_name,
                 tags,
                 color,
                 price,
-                allergens: HashSet::new(),
+                allergens,
             });
         }
         groups.push(MealGroup {
@@ -152,5 +261,5 @@ pub fn extract_response(html: &str) -> MenuResponse {
             meals,
         });
     }
-    MenuResponse { groups }
+    Ok(MenuResponse { groups })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate reqwest;
+extern crate chrono;
 mod lib;
 
 struct MenuQuery {
@@ -9,23 +10,31 @@ struct MenuQuery {
 
 struct MensaCode(u16);
 
-fn main() {
-    let mensa_code = MensaCode(191);
-    let params = [
-        ("week", "now"),
-        ("date", "2018-12-18"),
-        ("resources_id", &mensa_code.0.to_string()),
-    ];
-    let mut response = reqwest::Client::new()
+fn menu_html(
+    date: chrono::Date<chrono::Local>,
+    mensa_code: MensaCode,
+) -> reqwest::Result<reqwest::Response> {
+    reqwest::Client::new()
         .post("https://www.stw.berlin/xhr/speiseplan-wochentag.html")
-        .form(&params)
+        .form(
+            &[
+                ("week", "now"),
+                ("date", &date.format("%Y-%m-%d").to_string()),
+                ("resources_id", &mensa_code.0.to_string()),
+            ],
+        )
         .header(reqwest::header::USER_AGENT, "Mozilla/5.0")
-        .send().unwrap();
+        .send()
+}
+
+fn main() {
+    let mut response = menu_html(
+        chrono::Local::today(),
+        MensaCode(191),
+    ).unwrap();
     assert!(response.status().is_success());
 
-    println!("{:?}", response.text().unwrap());
+    let menu_response = lib::extract_response(&response.text().unwrap()).unwrap();
 
-    let menu_response = lib::extract_response(&response.text().unwrap());
-
-    println!("{:?}", menu_response);
+    println!("{}", menu_response);
 }
