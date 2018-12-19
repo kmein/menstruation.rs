0f4e256f621686b964c41322924814de1c122a35
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2018-12-19 23:53:12 +0100

    refactor; command line parsing

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,13 @@ extern crate ansi_term;
 extern crate regex;
 extern crate scraper;
 
-use std::collections;
+use ansi_term::{Colour, Style};
+use regex::Regex;
+use scraper::{html::Html, ElementRef, Selector};
+use std::collections::HashSet;
 use std::convert::TryFrom;
-use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
 
 mod utility {
     pub fn partition<A, P, I>(predicate: P, xs: I) -> (Vec<A>, Vec<A>)
@@ -26,7 +30,7 @@ mod utility {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
 pub struct Cents(u64);
 
 impl Cents {
@@ -35,8 +39,8 @@ impl Cents {
     }
 }
 
-impl fmt::Display for Cents {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl Display for Cents {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         let Cents(total_cents) = self;
         let euros = total_cents / 100;
         let cents = total_cents % 100;
@@ -44,55 +48,61 @@ impl fmt::Display for Cents {
     }
 }
 
-#[derive(Debug)]
-pub struct MenuResponse {
-    groups: Vec<MealGroup>,
-}
-
-impl fmt::Display for MenuResponse {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for group in &self.groups {
-            write!(f, "{}", group);
+impl FromStr for Cents {
+    type Err = std::num::ParseFloatError;
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        match string.parse() {
+            Ok(float) => Ok(Cents::from_euro(float)),
+            Err(err) => Err(err),
         }
-        Ok(())
-    }
-}
-
-impl From<scraper::html::Html> for MenuResponse {
-    fn from(html: scraper::html::Html) -> Self {
-        let group_selector = scraper::Selector::parse(".splGroupWrapper").unwrap();
-        let groups = html.select(&group_selector).map(MealGroup::from).collect();
-        MenuResponse { groups }
     }
 }
 
 #[derive(Debug)]
-struct MealGroup {
-    name: String,
-    meals: Vec<Meal>,
+pub struct MenuResponse(pub Vec<MealGroup>);
+
+impl Display for MenuResponse {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        let mut result = Ok(());
+        for group in &self.0 {
+            result = write!(f, "{}", group);
+        }
+        result
+    }
 }
 
-impl fmt::Display for MealGroup {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl From<Html> for MenuResponse {
+    fn from(html: Html) -> Self {
+        let group_selector = Selector::parse(".splGroupWrapper").unwrap();
+        let groups = html.select(&group_selector).map(MealGroup::from).collect();
+        MenuResponse(groups)
+    }
+}
+
+#[derive(Debug)]
+pub struct MealGroup {
+    pub name: String,
+    pub meals: Vec<Meal>,
+}
+
+impl Display for MealGroup {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         writeln!(
             f,
             "{}",
-            ansi_term::Style::new()
-                .bold()
-                .paint(&self.name.to_uppercase(),)
+            Style::new().bold().paint(&self.name.to_uppercase(),)
         );
         for meal in &self.meals {
             write!(f, "{}", meal);
         }
-        writeln!(f, "");
-        Ok(())
+        writeln!(f)
     }
 }
 
-impl From<scraper::ElementRef<'_>> for MealGroup {
-    fn from(html: scraper::ElementRef<'_>) -> Self {
-        let group_name_selector = scraper::Selector::parse(".splGroup").unwrap();
-        let meal_selector = scraper::Selector::parse(".splMeal").unwrap();
+impl From<ElementRef<'_>> for MealGroup {
+    fn from(html: ElementRef<'_>) -> Self {
+        let group_name_selector = Selector::parse(".splGroup").unwrap();
+        let meal_selector = Selector::parse(".splMeal").unwrap();
 
         let group_name = html
             .select(&group_name_selector)
@@ -108,21 +118,21 @@ impl From<scraper::ElementRef<'_>> for MealGroup {
 }
 
 #[derive(Debug)]
-struct Meal {
-    name: String,
-    color: MealColor,
-    tags: collections::HashSet<MealTag>,
-    price: Option<MealPrice>,
-    allergens: collections::HashSet<String>,
+pub struct Meal {
+    pub name: String,
+    pub color: MealColor,
+    pub tags: HashSet<MealTag>,
+    pub price: Option<MealPrice>,
+    pub allergens: HashSet<String>,
 }
 
-impl fmt::Display for Meal {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        fn to_ansi(color: &MealColor) -> ansi_term::Colour {
+impl Display for Meal {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        fn to_ansi(color: &MealColor) -> Colour {
             match color {
-                MealColor::Green => ansi_term::Colour::Green,
-                MealColor::Red => ansi_term::Colour::Red,
-                MealColor::Yellow => ansi_term::Colour::Yellow,
+                MealColor::Green => Colour::Green,
+                MealColor::Red => Colour::Red,
+                MealColor::Yellow => Colour::Yellow,
             }
         }
         writeln!(
@@ -143,18 +153,22 @@ impl fmt::Display for Meal {
     }
 }
 
-impl From<scraper::ElementRef<'_>> for Meal {
-    fn from(html: scraper::ElementRef<'_>) -> Self {
-        let icon_selector = scraper::Selector::parse("img.splIcon").unwrap();
-        let meal_name_selector = scraper::Selector::parse("span.bold").unwrap();
-        let allergen_selector = scraper::Selector::parse(".toolt").unwrap();
+impl From<ElementRef<'_>> for Meal {
+    fn from(html: ElementRef<'_>) -> Self {
+        let icon_selector = Selector::parse("img.splIcon").unwrap();
+        let meal_name_selector = Selector::parse("span.bold").unwrap();
+        let allergen_selector = Selector::parse(".toolt").unwrap();
 
         let icons_html = html
             .select(&icon_selector)
             .map(|img| img.value().attr("src").expect("Icon has no src"));
         let (color_htmls, tag_htmls) = utility::partition(|&src| src.contains("ampel"), icons_html);
-        let color = MealColor::from(color_htmls[0]);
-        let tags = tag_htmls.iter().map(|&src| MealTag::from(src)).collect();
+        let color = parse_color(color_htmls[0]).expect("Unknown color icon");
+        let tags = tag_htmls
+            .iter()
+            .map(|&src| parse_tag(src))
+            .collect::<Option<HashSet<_>>>()
+            .expect("Unknown tag icon");
         let meal_name = html
             .select(&meal_name_selector)
             .next()
@@ -164,7 +178,7 @@ impl From<scraper::ElementRef<'_>> for Meal {
             .to_string();
         let price = MealPrice::try_from(html).ok();
         let allergens = {
-            let parenthesized = regex::Regex::new(r"\((.*)\)").unwrap();
+            let parenthesized = Regex::new(r"\((.*)\)").unwrap();
             let allergens_html = html
                 .select(&allergen_selector)
                 .next()
@@ -176,7 +190,7 @@ impl From<scraper::ElementRef<'_>> for Meal {
                     .map(String::from)
                     .collect()
             } else {
-                collections::HashSet::new()
+                HashSet::new()
             }
         };
         Meal {
@@ -196,14 +210,27 @@ pub enum MealColor {
     Red,
 }
 
-impl From<&str> for MealColor {
-    fn from(uri: &str) -> Self {
-        match uri {
-            "/vendor/infomax/mensen/icons/ampel_gelb_70x65.png" => MealColor::Yellow,
-            "/vendor/infomax/mensen/icons/ampel_gruen_70x65.png" => MealColor::Green,
-            "/vendor/infomax/mensen/icons/ampel_rot_70x65.png" => MealColor::Red,
-            _ => panic!("Cannot parse URI: {}", uri),
+impl FromStr for MealColor {
+    type Err = String;
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        match string {
+            "grün" => Ok(MealColor::Green),
+            "gelb" => Ok(MealColor::Yellow),
+            "rot" => Ok(MealColor::Red),
+            _ => Err(format!(
+                "Falsche Farbe: {}. Bitte nutze grün, gelb oder rot.",
+                string
+            )),
         }
+    }
+}
+
+fn parse_color(uri: &str) -> Option<MealColor> {
+    match uri {
+        "/vendor/infomax/mensen/icons/ampel_gelb_70x65.png" => Some(MealColor::Yellow),
+        "/vendor/infomax/mensen/icons/ampel_gruen_70x65.png" => Some(MealColor::Green),
+        "/vendor/infomax/mensen/icons/ampel_rot_70x65.png" => Some(MealColor::Red),
+        _ => None,
     }
 }
 
@@ -216,12 +243,29 @@ pub enum MealTag {
     ClimateFriendly,
 }
 
-impl fmt::Display for MealTag {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl FromStr for MealTag {
+    type Err = String;
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        match string {
+            "vegan" => Ok(MealTag::Vegan),
+            "vegetarisch" => Ok(MealTag::Vegetarian),
+            "bio" => Ok(MealTag::Organic),
+            "öko" => Ok(MealTag::ClimateFriendly),
+            "nachhaltig" => Ok(MealTag::SustainableFishing),
+            _ => Err(format!(
+                "Falsches Tag: {}. Bitte nutze vegan, vegetarisch, bio, öko oder nachhaltig.",
+                string
+            )),
+        }
+    }
+}
+
+impl Display for MealTag {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         write!(
             f,
             "{}",
-            ansi_term::Style::new().italic().paint(match self {
+            Style::new().italic().paint(match self {
                 MealTag::Vegetarian => "vegetarisch",
                 MealTag::Vegan => "vegan",
                 MealTag::Organic => "bio",
@@ -232,37 +276,35 @@ impl fmt::Display for MealTag {
     }
 }
 
-impl From<&str> for MealTag {
-    fn from(uri: &str) -> Self {
-        match uri {
-            "/vendor/infomax/mensen/icons/1.png" => MealTag::Vegetarian,
-            "/vendor/infomax/mensen/icons/15.png" => MealTag::Vegan,
-            "/vendor/infomax/mensen/icons/18.png" => MealTag::Organic,
-            "/vendor/infomax/mensen/icons/38.png" => MealTag::SustainableFishing,
-            "/vendor/infomax/mensen/icons/43.png" => MealTag::ClimateFriendly,
-            _ => panic!("Cannot parse URI: {}", uri),
-        }
+fn parse_tag(uri: &str) -> Option<MealTag> {
+    match uri {
+        "/vendor/infomax/mensen/icons/1.png" => Some(MealTag::Vegetarian),
+        "/vendor/infomax/mensen/icons/15.png" => Some(MealTag::Vegan),
+        "/vendor/infomax/mensen/icons/18.png" => Some(MealTag::Organic),
+        "/vendor/infomax/mensen/icons/38.png" => Some(MealTag::SustainableFishing),
+        "/vendor/infomax/mensen/icons/43.png" => Some(MealTag::ClimateFriendly),
+        _ => None,
     }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash)]
-struct MealPrice {
-    student: Cents,
+pub struct MealPrice {
+    pub student: Cents,
     employee: Cents,
     guest: Cents,
 }
 
-impl TryFrom<scraper::ElementRef<'_>> for MealPrice {
+impl TryFrom<ElementRef<'_>> for MealPrice {
     type Error = ();
-    fn try_from(html: scraper::ElementRef<'_>) -> Result<Self, ()> {
-        let price_selector = scraper::Selector::parse("div.text-right").unwrap();
+    fn try_from(html: ElementRef<'_>) -> Result<Self, ()> {
+        let price_selector = Selector::parse("div.text-right").unwrap();
         if let Some(price_raw) = html.select(&price_selector).next() {
             let prices: Vec<_> = price_raw
                 .inner_html()
                 .replace("€", "")
                 .trim()
                 .replace(",", ".")
-                .split("/")
+                .split('/')
                 .map(|p| Cents::from_euro(p.parse::<f32>().expect("Could not parse price")))
                 .collect();
             Ok(MealPrice {
@@ -277,5 +319,5 @@ impl TryFrom<scraper::ElementRef<'_>> for MealPrice {
 }
 
 pub fn extract(html: &str) -> MenuResponse {
-    MenuResponse::from(scraper::html::Html::parse_fragment(html))
+    MenuResponse::from(Html::parse_fragment(html))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,123 @@
 #![feature(try_from)]
 extern crate chrono;
+extern crate menstruation;
 extern crate reqwest;
-mod lib;
+#[macro_use]
+extern crate structopt;
 
-struct MenuQuery {
-    max_price: Option<lib::Cents>,
-    allowed_colors: Option<Vec<lib::MealColor>>,
-    allowed_tags: Option<Vec<lib::MealTag>>,
+use chrono::{Local, NaiveDate, ParseError};
+use menstruation::*;
+use reqwest::{header, Client, Response};
+use std::str::FromStr;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    rename_all = "kebab-case",
+    raw(setting = "structopt::clap::AppSettings::ColoredHelp")
+)]
+struct Options {
+    /// Display only meals with the specified colors
+    #[structopt(short, long, parse(try_from_str))]
+    colors: Vec<MealColor>,
+
+    /// Display only meals with the specified tags
+    #[structopt(short, long, parse(try_from_str))]
+    tags: Vec<MealTag>,
+
+    /// Display no meals more expensive than a given price
+    #[structopt(short = "p", long)]
+    max_price: Option<Cents>,
+
+    /// Display a given date's menu
+    #[structopt(short, long, parse(try_from_str = "parse_iso_date"))]
+    date: Option<NaiveDate>,
+
+    /// Display a given mensa's menu
+    #[structopt(short, long, default_value = "191")]
+    mensa: MensaCode,
 }
 
+fn parse_iso_date(string: &str) -> Result<NaiveDate, ParseError> {
+    NaiveDate::parse_from_str(string, "%Y-%m-%d")
+}
+
+#[derive(Debug)]
 struct MensaCode(u16);
 
-fn menu_html(
-    date: chrono::Date<chrono::Local>,
-    mensa_code: MensaCode,
-) -> reqwest::Result<reqwest::Response> {
-    reqwest::Client::new()
+impl FromStr for MensaCode {
+    type Err = std::num::ParseIntError;
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        match string.parse() {
+            Ok(number) => Ok(MensaCode(number)),
+            Err(err) => Err(err),
+        }
+    }
+}
+
+fn menu_html(mensa_code: &MensaCode, menu_date: Option<NaiveDate>) -> reqwest::Result<Response> {
+    Client::new()
         .post("https://www.stw.berlin/xhr/speiseplan-wochentag.html")
         .form(&[
             ("week", "now"),
-            ("date", &date.format("%Y-%m-%d").to_string()),
+            (
+                "date",
+                &menu_date
+                    .unwrap_or(Local::today().naive_local())
+                    .format("%Y-%m-%d")
+                    .to_string(),
+            ),
             ("resources_id", &mensa_code.0.to_string()),
         ])
-        .header(reqwest::header::USER_AGENT, "Mozilla/5.0")
+        .header(header::USER_AGENT, "Mozilla/5.0")
         .send()
 }
 
+fn filter_menu(options: &Options, menu: MenuResponse) -> MenuResponse {
+    fn matches_query(options: &Options, meal: &Meal) -> bool {
+        let price_ok = if let Some(max) = &options.max_price {
+            if let Some(price) = &meal.price {
+                price.student <= *max
+            } else {
+                false
+            }
+        } else {
+            true
+        };
+        let colors_ok = if options.colors.is_empty() {
+            true
+        } else {
+            options.colors.contains(&meal.color)
+        };
+        let tags_ok = if options.tags.is_empty() {
+            true
+        } else {
+            meal.tags.iter().any(|tag| options.tags.contains(tag))
+        };
+        price_ok && colors_ok && tags_ok
+    }
+
+    let mut groups = Vec::new();
+    for group in menu.0 {
+        let meals = group
+            .meals
+            .into_iter()
+            .filter(|meal| matches_query(options, meal))
+            .collect::<Vec<_>>();
+        if !meals.is_empty() {
+            groups.push(MealGroup { meals, ..group });
+        }
+    }
+    MenuResponse(groups)
+}
+
 fn main() {
-    let mut response = menu_html(chrono::Local::today(), MensaCode(191)).unwrap();
+    let options = Options::from_args();
+
+    let mut response = menu_html(&options.mensa, options.date).unwrap();
     assert!(response.status().is_success());
 
-    let menu_response = lib::extract(&response.text().unwrap());
+    let menu_response = extract(&response.text().unwrap());
 
-    println!("{}", menu_response);
+    println!("{}", filter_menu(&options, menu_response));
 }
