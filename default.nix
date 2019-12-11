0114262f965fbfdaeb60e165a2d84cf7dfe9df0d
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-12-11 17:26:26 +0100

    Update and rename shell.nix to default.nix

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ mozilla ? import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz)
+, pkgs ?  import <nixpkgs> { overlays = [ mozilla ]; }
+}:
+let
+  nightly = pkgs.rustChannelOf { date = "2019-06-20"; channel = "nightly"; };
+  nightly-rustPlatform = pkgs.makeRustPlatform {
+    rustc = nightly.rust;
+    cargo = nightly.cargo;
+  };
+in with pkgs; nightly-rustPlatform.buildRustPackage rec {
+  name = "menstruation-rs";
+
+  src = ./.;
+
+  nativeBuildInputs = [ pkgconfig ] ++ lib.optional (builtins.currentSystem == "x86_64-darwin") darwin.apple_sdk.frameworks.Security;
+
+  checkPhase = "";
+  cargoSha256 = "0r1rj9pb0ln95g6266afr43nygfnh891mqyh7mzk4n4y45vb5dv3";
+
+  buildInputs = [ openssl ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://github.com/kmein/menstruation.rs";
+    license = licenses.gpl3;
+    platforms = platforms.all;
+    maintainers = [ maintainers.kmein ];
+  };
+}
