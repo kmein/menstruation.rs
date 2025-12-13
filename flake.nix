cc7278fc187f1d4b1f802fbde926a6195a7a0d47
Kierán Meinhardt <kmein@posteo.de>
2025-12-13 21:20:54 +0100

    flake: remove flake-utils

--- flake.nix
+++ flake.nix
@@ -4,26 +4,31 @@
   inputs = {
     fenix.url = "github:nix-community/fenix";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    inputs:
-    with inputs;
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
+    {
+      self,
+      nixpkgs,
+      fenix,
+    }:
+    let
+      lib = nixpkgs.lib;
+      eachSupportedSystem = lib.genAttrs lib.systems.flakeExposed;
+      pkgsFor =
+        system:
+        import nixpkgs {
           inherit system;
           overlays = [ fenix.overlays.default ];
         };
-        rustPlatform = pkgs.makeRustPlatform {
-          rustc = pkgs.fenix.complete.withComponents [ "rustc" ];
-          cargo = pkgs.fenix.complete.withComponents [ "cargo" ];
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
+    in
+    {
+      devShell = eachSupportedSystem (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        pkgs.mkShell {
           buildInputs = [
             (pkgs.fenix.complete.withComponents [
               "cargo"
@@ -32,30 +37,42 @@
             pkgs.pkg-config
             pkgs.openssl
           ];
-        };
-        defaultPackage = self.packages.${system}.menstruation-backend;
-        packages.menstruation-backend = rustPlatform.buildRustPackage {
-          pname = "menstruation";
-          version = "0.1.0";
-          src = ./.;
-          cargoLock = {
-            lockFile = builtins.path {
-              path = self + "/Cargo.lock";
-              name = "Cargo.lock";
+        }
+      );
+      defaultPackage = eachSupportedSystem (system: self.packages.${system}.menstruation-backend);
+      packages = eachSupportedSystem (
+        system:
+        let
+          pkgs = pkgsFor system;
+          rustPlatform = pkgs.makeRustPlatform {
+            rustc = pkgs.fenix.complete.withComponents [ "rustc" ];
+            cargo = pkgs.fenix.complete.withComponents [ "cargo" ];
+          };
+        in
+        {
+          menstruation-backend = rustPlatform.buildRustPackage {
+            pname = "menstruation";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock = {
+              lockFile = builtins.path {
+                path = self + "/Cargo.lock";
+                name = "Cargo.lock";
+              };
+              allowBuiltinFetchGit = true;
             };
-            allowBuiltinFetchGit = true;
-          };
 
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs = [ pkgs.openssl ];
-          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-          meta = with pkgs.lib; {
-            homepage = "https://github.com/kmein/menstruation.rs";
-            license = licenses.gpl3;
-            platforms = platforms.all;
-            maintainers = [ maintainers.kmein ];
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl ];
+            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+            meta = with pkgs.lib; {
+              homepage = "https://github.com/kmein/menstruation.rs";
+              license = licenses.gpl3;
+              platforms = platforms.all;
+              maintainers = [ maintainers.kmein ];
+            };
           };
-        };
-      }
-    );
+        }
+      );
+    };
 }
